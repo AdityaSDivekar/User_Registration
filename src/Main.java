5095c849ea1ef0f3e5c0ea74217394cf2879940f
AdityaSDivekar <adityadivekar1712@email.com>
2024-12-03 11:26:00 +0530

    validate user name with regex

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.regex.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to user registration");
+
+    public static boolean validateFirstName(String firstName) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(firstName);
+
+        return matcher.matches();
     }
 
+    public static void main(String[] args) {
+        System.out.println(validateFirstName("John"));
+        System.out.println(validateFirstName("jo"));
+        System.out.println(validateFirstName("Mary"));
+        System.out.println(validateFirstName("mike"));
+    }
 }
